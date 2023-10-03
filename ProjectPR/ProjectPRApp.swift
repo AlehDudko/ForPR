dbdd8e6efdf555f25319622b71614a1d79a6e25b
Aleh <aleh.dudko@cogniteq.com>
2023-10-03 23:59:06 +0300

    Initiate project

---
+++ ProjectPR/ProjectPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPRApp.swift
+//  ProjectPR
+//
+//  Created by Aleh Dudko on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
